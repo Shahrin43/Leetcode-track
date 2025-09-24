7b6fdbbee0323c0f0308d3b55ce39f3b56fe54d1
Shahrin43 <165501604+Shahrin43@users.noreply.github.com>
2025-09-23 22:35:14 -0400

    Time: 7 ms (67.95%) | Memory: 30.2 MB (30.07%) - LeetSync

---
+++ 143-reorder-list/reorder-list.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+#Finiding the middle point using the hare and tortoise algorithm
+        fast, slow= head.next, head
+
+        while fast and fast.next:
+            slow= slow.next
+            fast= fast.next.next
+        
+        second= slow.next
+        prev= slow.next=None
+#reversing the second half of the list
+        while second:
+            temp= second.next
+            second.next= prev
+            prev= second
+            second= temp
+#now merging the two halves of the list
+        first, second= head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+        
